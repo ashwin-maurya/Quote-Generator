b383fad8cabe4eee3a1644d1a02be976dcc535c0
Ashwin Maurya <ashwinmaurya30@gmail.com>
2023-11-10 17:58:57 +0530

    Frotend halfway complete, basic frontend temaplte complete

--- components/QuoteGenerator/QuoteGeneratorElements.tsx
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 import Image from "next/image";
 
 export const GradientBackgroundCon = styled.div`
-  background: linear-gradient(to right, #910000, #f3ae00);
+  background: linear-gradient(to right, #270091, #f3ae00);
   background-size: 400% 400%;
   animation: gradient 6s ease infinite;
   height: 100vh;
   width: 100vw;
+  display: flex;
+  justify-content: space-between;
   @keyframes gradient {
     0% {
       background-position: 0% 50%;
@@ -19,4 +21,145 @@ export const GradientBackgroundCon = styled.div`
       background-position: 0% 50%;
     }
   }
+`;
+
+export const BackgroundImage1 = styled(Image)`
+  position: relative;
+  z-index: 1;
+  padding: 20px;
+  margin-left: -10px;
+  margin-top: -10px;
+`;
+export const BackgroundImage2 = styled(Image)`
+  position: fixed;
+  z-index: 1;
+  padding: 20px;
+  right: -10px;
+  bottom: -10px;
+`;
+
+export const FootCon = styled.div`
+  width: 100vw;
+  height: 50px;
+  text-align: center;
+  font-family: "Source Code pro", monospace;
+  font-size: 15px;
+  position: absolute;
+  bottom: 0;
+  color: white;
+  z-index: 999999;
+`;
+
+export const FooterLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  transition: 0.4s;
+  &:hover {
+    color: #ffffffd7;
+  }
+`;
+export const FloatingButton = styled(Link)`
+  color: white;
+  transition: 0.4s;
+  padding: 10px 20px;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 20%);
+  background: rgba(255, 255, 255, 0.2);
+  box-shadow: 0 8px 32px 0 rgba(232, 232, 232, 0.2);
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+  border-radius: 5px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  z-index: 9999999;
+  &:hover {
+    color: #ffffffd7;
+  }
+`;
+
+export const QuoteGeneratorCon = styled.div`
+  min-height: 350px;
+  min-width: 350px;
+  height: 70vh;
+  width: 70vw;
+  border: 2px solid #ffffff22;
+  border-radius: 15px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  z-index: 2;
+  background: rgba(255, 255, 255, 0.2);
+  box-shadow: 0 8px 32px 0 rgba(232, 232, 232, 0.2);
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+`;
+
+export const QuoteGeneratorInnerCon = styled.div`
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 100%;
+`;
+export const QuoteGeneratorTitle = styled.div`
+  font-family: "Permanent Marker", cursive;
+  font-size: 50px;
+  text-align: center;
+  color: white;
+  padding: 0px 20px 0px 20px;
+  position: relative;
+  /* media query */
+  @media only screen and (max-width: 800px) {
+    font-size: 30px;
+  }
+`;
+
+export const QuoteGeneratorSubTitle = styled.div`
+  color: white;
+  font-family: "Caveat", cursive;
+  font-size: 35px;
+  position: relative;
+  width: 100%;
+  word-wrap: break-word;
+  text-align: center;
+  padding: 0px 20px 0px 20px;
+  @media only screen and (max-width: 800px) {
+    font-size: 25px;
+  }
+`;
+
+export const GenerateQuoteButton = styled.div`
+  border: 2px solid darkgrey;
+  border-radius: 20px;
+  margin-top: 20px;
+  padding: 10px 40px;
+  transition: 0.2s all ease-in-out;
+  cursor: pointer;
+  margin: auto;
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transform-origin: center;
+  background: rgba(0, 0, 70, 0.3);
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  &:hover {
+    filter: brightness(2);
+  }
+`;
+
+export const GenerateQuoteButtonText = styled.div`
+  color: white;
+  font-family: "Caveat", cursive;
+  font-size: 35px;
+  width: 100%;
+  text-align: center;
 `;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,31 @@
+import React, { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import { GradientBackgroundCon } from "@/components/QuoteGenerator/QuoteGeneratorElements";
+
+//components
+import {
+  BackgroundImage1,
+  BackgroundImage2,
+  GradientBackgroundCon,
+  FooterLink,
+  QuoteGeneratorCon,
+  FootCon,
+  QuoteGeneratorInnerCon,
+  QuoteGeneratorTitle,
+  QuoteGeneratorSubTitle,
+  GenerateQuoteButton,
+  GenerateQuoteButtonText,
+  FloatingButton,
+} from "@/components/QuoteGenerator/QuoteGeneratorElements";
+
+//assets
+import Cloud1 from "../assets/cloud-and-thunder.png";
+import Cloud2 from "../assets/cloudy-weather.png";
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -14,7 +36,55 @@ export default function Home() {
       </Head>
 
       {/* BackGround */}
-      <GradientBackgroundCon />
+      <GradientBackgroundCon>
+        {/* Quote Generator MOdal */}
+        {/* <QuoteGeneratorModal/> */}
+
+        {/* Quote Generator */}
+        <FloatingButton
+          href="https://www.linkedin.com/in/ashwin-maurya/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by ZentQutoes
+        </FloatingButton>
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>
+              Daily Inspiration Generator
+            </QuoteGeneratorTitle>
+            <QuoteGeneratorSubTitle>
+              Looking for an inspiration? Generate a Quote here and get started
+              now!!
+            </QuoteGeneratorSubTitle>
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={null}>
+                Make a Quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+        {/* Background Images */}
+        <BackgroundImage1 src={Cloud1} height="300" alt="Cloud Image" />
+        <BackgroundImage2 src={Cloud2} height="300" alt="Cloud Image" />
+
+        {/* Footer */}
+        <FootCon>
+          <>
+            Quotes Genreated: {numberOfQuotes}
+            <br />
+            Developed with ❤️ by{" "}
+            <FooterLink
+              href="https://www.linkedin.com/in/ashwin-maurya/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @ashwin
+            </FooterLink>
+          </>
+        </FootCon>
+      </GradientBackgroundCon>
     </>
   );
 }
